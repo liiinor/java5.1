d72e2120c4c4dbbd50d44035c0d8224ebff648c8
lina mayakovskaya <linamayakovskaya@mail.ru>
2023-07-04 16:44:33 +0500

    Initial commit

--- src/test/java/ru/netology/services/java5/MonthsOfRestServiceTest.java
+++ src/test/java/ru/netology/services/java5/MonthsOfRestServiceTest.java
@@ -1,13 +1,15 @@
 package ru.netology.services.java5;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class MonthsOfRestServiceTest {
-    @ParametrizedTest
+
+    @ParameterizedTest
     @CsvSource({
-            "3,10_000,3000,20_000",
-            "2,100_000,60_000,150_000"
+            "3,10000,3000,20000",
+            "2,100000,60000,150000"
     })
 
     public void MonthsOfRestService(int expected, int income, int expense, int threshold) {
